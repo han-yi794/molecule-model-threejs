af953f1b795b15e761361845b6357a32f1da5440
dev <dev@local>
2026-08-23 22:18:17 +0800

    chore: 清理调试脚本

--- check_sidebar_after.js
+++
@@ -1,7 +0,0 @@
-// check_sidebar_after.js
-const fs = require('fs');
-const s = fs.readFileSync('球棍模型4.html', 'utf8');
-const i = s.indexOf('id="btn-optimize-all"');
-// 看看当前 sidebar 中 optimize 附近的代码
-const seg = s.slice(i, i + 1500);
-console.log(seg);
--- find_grid_handler.js
+++
@@ -1,5 +0,0 @@
-// find_grid_handler.js
-const fs = require('fs');
-const s = fs.readFileSync('球棍模型4.html', 'utf8');
-const i = s.indexOf('btn-toggle-grid');
-console.log(s.slice(i, i + 2000));
